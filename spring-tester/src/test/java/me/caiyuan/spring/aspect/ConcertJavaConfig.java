3866dd6457caecfbf4824e47d06f0910a04ae3ae
caiyuan <caiyuan@live.com>
2016-06-30 09:37:16 +0800

    spring-tester

--- spring-tester/src/test/java/me/caiyuan/spring/aspect/ConcertJavaConfig.java
+++ spring-tester/src/test/java/me/caiyuan/spring/aspect/ConcertJavaConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+// @EnableAspectJAutoProxy 启用 AspectJ 自动代理
+
 /**
  * YUAN
  * 6/29/16.
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan
 public class ConcertJavaConfig {
 
+    // 声明 Audience bean
     /*
     @Bean
     public Audience audience() {
