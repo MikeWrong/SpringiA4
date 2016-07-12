c81009a3c3d69b5ad657890ac8f2c458db71c948
caiyuan <caiyuan@live.com>
2016-07-12 15:23:16 +0800

    Spring in Action, Fourth Edition

--- SpringWeb/spring-web/src/main/java/me/caiyuan/spring/web/repository/JdbcSpittleRepository.java
+++ SpringWeb/spring-web/src/main/java/me/caiyuan/spring/web/repository/JdbcSpittleRepository.java
@@ -1,5 +1,7 @@
 package me.caiyuan.spring.web.repository;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import java.util.List;
  * YUAN
  * 7/12/16.
  */
+@Repository
 public class JdbcSpittleRepository implements SpittleRepository {
 
     public List<Spittle> findSpittles(long max, int count) {
