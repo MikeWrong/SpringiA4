5779f526159596445d1a3c05575dfd4472435e28
caiyuan <caiyuan@live.com>
2016-07-14 16:46:56 +0800

    Spring in Action, Fourth Edition

--- SpringWeb/spring-jsp/src/main/java/me/caiyuan/spring/web/controller/SpitterController.java
+++ SpringWeb/spring-jsp/src/main/java/me/caiyuan/spring/web/controller/SpitterController.java
@@ -21,20 +21,21 @@ public class SpitterController {
     @RequestMapping(value = "register", method = GET)
     public String showRegistrationForm(Model model) {
         Spitter defaultSpitter = new Spitter();
+        // 设置表单的默认值
         defaultSpitter.setUsername("Guest");
+        // 在模型中必须有一个key为spitter的对象,否则的话,表单不能正常渲染(会出现JSP错误 java.lang.IllegalStateException: Neither BindingResult nor plain target object for bean name 'spitter' available as request attribute)
         model.addAttribute(defaultSpitter);
         return "registerForm";
     }
 
     @RequestMapping(value = "register", method = POST)
     public String showRegistrationForm(@Valid Spitter spitter, Errors errors) {
-        System.out.println(spitter);
-        System.out.println(errors);
-
+        // 校验错误处理
         if (errors.hasErrors()) {
             return "registerForm";
         }
-
+        // 保存 Spitter
+        // ...
         return "redirect:register";
     }
 
