df4a15efd855e107a4e50a59ba31e5b377a86c9a
caiyuan <caiyuan@live.com>
2016-07-15 23:27:38 +0800

    Spring in Action, Fourth Edition

--- SpringWeb/spring-tiles/src/main/java/me/caiyuan/spring/web/config/WebConfig.java
+++ SpringWeb/spring-tiles/src/main/java/me/caiyuan/spring/web/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
