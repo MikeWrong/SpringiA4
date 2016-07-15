f4997076130b8842cdb9f5fbf519dfb6209ca4b3
caiyuan <caiyuan@live.com>
2016-07-15 22:31:16 +0800

    Spring in Action, Fourth Edition

--- SpringWeb/spring-tiles/src/main/java/me/caiyuan/spring/web/config/WebConfig.java
+++ SpringWeb/spring-tiles/src/main/java/me/caiyuan/spring/web/config/WebConfig.java
@@ -6,12 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
@@ -25,18 +23,6 @@ import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 // 启用组件扫描
 @ComponentScan(basePackageClasses = WebPackage.class)
 public class WebConfig extends WebMvcConfigurerAdapter {
-
-    // 配置JSP视图解析器
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver resolver =
-                new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
-    }
 
     @Bean
     public MessageSource messageSource() {
@@ -63,6 +49,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
     }
 
 }
--- SpringWeb/spring-tiles/src/main/webapp/WEB-INF/views/registerForm.jsp
+++ SpringWeb/spring-tiles/src/main/webapp/WEB-INF/views/registerForm.jsp
@@ -1,24 +1,25 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="sf" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
 <%@ page session="false" %>
-<h1>Register</h1>
+<h1><s:message code="page.register"/></h1>
 
-<sf:form method="POST" commandName="spitter" >
-    <sf:errors path="*" element="div" cssClass="errors" />
+<sf:form method="POST" commandName="spitter">
+    <sf:errors path="*" element="div" cssClass="errors"/>
     <sf:label path="firstName"
               cssErrorClass="error">First Name</sf:label>:
-    <sf:input path="firstName" cssErrorClass="error" /><br/>
+    <sf:input path="firstName" cssErrorClass="error"/><br/>
     <sf:label path="lastName"
               cssErrorClass="error">Last Name</sf:label>:
-    <sf:input path="lastName" cssErrorClass="error" /><br/>
+    <sf:input path="lastName" cssErrorClass="error"/><br/>
     <sf:label path="email"
               cssErrorClass="error">Email</sf:label>:
-    <sf:input path="email" cssErrorClass="error" /><br/>
+    <sf:input path="email" cssErrorClass="error"/><br/>
     <sf:label path="username"
               cssErrorClass="error">Username</sf:label>:
-    <sf:input path="username" cssErrorClass="error" /><br/>
+    <sf:input path="username" cssErrorClass="error"/><br/>
     <sf:label path="password"
               cssErrorClass="error">Password</sf:label>:
-    <sf:password path="password" cssErrorClass="error" /><br/>
-    <input type="submit" value="Register" />
+    <sf:password path="password" cssErrorClass="error"/><br/>
+    <input type="submit" value="Register"/>
 </sf:form>
